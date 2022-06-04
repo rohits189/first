6626ba0fcdb655f0b57d49769d93513b5e599310
rohits189 <106867190+rohits189@users.noreply.github.com>
2022-06-04 11:22:19 +0100

    Uploaded uncheck py code
    This is first test check in

---
+++ uncheck.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    print('Adding numbers uncheck')
+    z = x + y
+    return z
+   
+def sub(x, y):
+    print('Subtracting number uncheck')
+    z = x - y
+    return z
+    
+x = 90
+d = {'Name': 'Add', 'Module':1}
